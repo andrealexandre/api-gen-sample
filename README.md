23cd0e39233f8c197f7eeeb893647f36575decad
Andre Alexandre <andre.alexandre@workday.com>
2022-06-29 16:04:16 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# API Codegen
+
+API Codegen for Scala + Akka-HTTP. 
+
+## How to run server
+
+* Generate code - `./gradlew guardrail` (code in `build/guardrail-sources`)
+* Run server - `./gradlew run`
+
+## Helpful curl commands
+
+* Get all users
+  * `curl -s -XGET "localhost:1234/users" | jq`
+* Get all users
+  * `curl -s -XGET "localhost:1234/users/0001" | jq`
+* Add user to server
+  * `curl -s -XPOST "localhost:1234/users" -H "Content-Type: application/json" --data '{ "id": "0003", "name": "User3" }' | jq`
---
+++ src/main/scala/api/gen/HttpServer.scala
@@ -0,0 +1,60 @@
+package api.gen
+
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.server.Route
+import api.gen.sample.Users.{UsersHandler, UsersResource}
+import api.gen.sample.definitions.{Problem, User}
+
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, ExecutionContextExecutor, Future}
+
+object HttpServer extends App {
+
+  implicit val system: ActorSystem[Nothing] =
+    ActorSystem(Behaviors.empty, "my-server-system")
+  implicit val executionContext: ExecutionContextExecutor =
+    system.executionContext
+
+  var db: Map[String, User] =
+    Seq(User("0001", "User1"), User("0002", "User2")).map(e => e.id -> e).toMap
+
+  val userRoutes = UsersResource.routes(new UsersHandler {
+    override def listUsers(respond: UsersResource.ListUsersResponse.type)(
+      limit: Option[Int]
+    ): Future[UsersResource.ListUsersResponse] = Future.successful {
+      respond.OK(db.values.toVector)
+    }
+
+    override def createUser(respond: UsersResource.CreateUserResponse.type)(
+      body: User
+    ): Future[UsersResource.CreateUserResponse] = Future.successful {
+      db += (body.id -> body)
+      respond.Created
+    }
+
+    override def getUser(respond: UsersResource.GetUserResponse.type)(
+      userId: String
+    ): Future[UsersResource.GetUserResponse] = Future.successful {
+      db.get(userId) match {
+        case Some(user) => respond.OK(user)
+        case None =>
+          respond.NotFound(Problem(Some(404), StatusCodes.NotFound.reason))
+      }
+    }
+  })
+
+  val routes: Route = Route.seal(userRoutes)
+
+  val bindingFuture: Future[Http.ServerBinding] =
+    Http().newServerAt("localhost", 1234).bind(routes)
+
+  Runtime.getRuntime.addShutdownHook(new Thread(() => {
+    bindingFuture.flatMap(_.unbind()).onComplete(_ => system.terminate())
+  }))
+
+  val binding = Await.result(bindingFuture, Duration.Inf)
+  println(s"Server now online. Please navigate to ${binding.localAddress}")
+}
